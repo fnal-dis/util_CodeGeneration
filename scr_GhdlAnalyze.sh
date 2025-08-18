a1997fad4b329a0197a840957ce34b4fd9ec784d
Javier Contreras <javierc@fnal.gov>
2025-08-18 12:24:49 -0500

    Clean up main branch for minimal clones

--- scr_GhdlAnalyze.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-cd src
-VENDOR_LIB_PATH=/home/javierc/xilinx-vivado/
-
-find . -name \*.cf -exec rm -rf {} \;
-find . -name pkg\*.vhd -exec ghdl -a {} \;
-find . -name \*.vhd -not -name top\* -exec ghdl -a {} \;
-find . -name top\*.vhd -exec ghdl -a -P${VENDOR_LIB_PATH} {} \;
